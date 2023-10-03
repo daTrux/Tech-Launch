84a840cce1b86df0ba658451449fc9030a27d3b3
Riccardo Turri <riccardo.turri.ext@siemens.com>
2023-10-03 14:37:27 +0200

    added shell to project

--- shell/src/app/app.component.spec.ts
+++ shell/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Tech'`, () => {
+  it(`should have as title 'shell'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Tech');
+    expect(app.title).toEqual('shell');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Tech app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('shell app is running!');
   });
 });
