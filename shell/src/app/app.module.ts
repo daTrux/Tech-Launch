9136961b7fb1fd5af1c035f3a20a33bf4ffb7222
Federico Gioeli <federico.gioeli.ext@siemens.com>
2023-10-13 14:36:48 +0200

    feat: add event registry state and message-bus

--- shell/src/app/app.module.ts
+++ shell/src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CityListsComponent } from './city-lists/city-lists.component';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NgxsModule} from '@ngxs/store';
+import {environment} from "../environments/environment";
+import {EventRegistryState} from "./event-registry-store/event-registry.state";
+import {noop} from "rxjs";
+import {CityListsComponent} from './city-lists/city-lists.component';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,19 @@ import { CityListsComponent } from './city-lists/city-lists.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxsModule.forRoot([EventRegistryState], {
+      developmentMode: !environment.production
+    })
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: noop,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ shell/src/app/event-registry-store/event-registry.state.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {Action, State, StateContext} from '@ngxs/store';
+import {Event} from "./event-registry.actions";
+
+export type EventHandler = (eventData: any) => any;
+
+export interface EventRegistryStateModel {
+  [eventName: string]: any
+}
+
+@State<EventRegistryStateModel>({
+  name: 'event-registry',
+  defaults: {}
+})
+@Injectable()
+export class EventRegistryState {
+
+  @Action(Event.Publish)
+  publishEvent(ctx: StateContext<any>, {eventName, eventData}: Event.Publish) {
+    const state = ctx.getState();
+    ctx.setState({
+      ...state,
+      [eventName]: eventData
+    });
+  }
+}
